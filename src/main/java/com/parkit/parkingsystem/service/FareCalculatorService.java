c84b7241c907b4fb6f083378658de95fdd975507
RedCoder42 <damien.jeanty@gmail.com>
2021-05-24 12:13:27 +0200

    Added comments to explain the new logic that makes the unit tests pass.

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -12,13 +12,20 @@ public class FareCalculatorService {
           "Out time provided is incorrect:" + ticket.getOutTime().toString());
     }
 
+    /*
+     * The logic to calculate the difference between two dates has been modified and this part of
+     * the code now uses new methods.
+     * 
+     * DateTime is used instead of integer variables to calculate the difference between the IN and
+     * OUT time. The difference is precisely calculated in minutes instead of hours. It then
+     * converts the difference in hours to calculate the Fare. DateTime is not deprecated while the
+     * methods used before were.
+     */
     DateTime dateTimeIn = new DateTime(ticket.getInTime());
     DateTime dateTimeOut = new DateTime(ticket.getOutTime());
     double diffInMinutes = dateTimeOut.getMinuteOfDay() - dateTimeIn.getMinuteOfDay();
     double diffInHours = (dateTimeOut.getDayOfYear() - dateTimeIn.getDayOfYear()) * 24;
     double duration = diffInHours + (diffInMinutes / 60);
-
-
 
     switch (ticket.getParkingSpot().getParkingType()) {
       case CAR: {
